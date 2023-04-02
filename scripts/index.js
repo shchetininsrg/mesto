53f5357723e18e4c67d4572f0b797e675fe9fa7b
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2023-04-02 22:36:56 +0500

    перенос

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 let editProfile = document.querySelector('.profile-info__edit-btn')
 let popup = document.querySelector('.popup')
-let popupClose = document.querySelector('.popup_close-btn')
+let popupClose = document.querySelector('.popup__close-btn')
+let profileInfoFullname = document.querySelector('.profile-info__fullname')
+let profileInfoDescription = document.querySelector('.profile-info__description')
+let popupInputFullname = document.querySelector('.popup__input_fullname')
+let popupInputDescription = document.querySelector('.popup__input_description')
+let formElement = document.querySelector('.popup__form')
+
+popupInputFullname.value = profileInfoFullname.innerHTML
+popupInputDescription.value = profileInfoDescription.innerHTML
 
 editProfile.onclick = function () {
     popup.classList.add('popup_open')
@@ -9,3 +17,12 @@ editProfile.onclick = function () {
 popupClose.onclick = function () {
     popup.classList.remove('popup_open')
 }
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileInfoFullname.textContent = popupInputFullname.value
+    profileInfoDescription.textContent = popupInputDescription.value 
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
